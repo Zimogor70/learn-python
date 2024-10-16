d73e610932a3eb7717a482b4676c9e7f48af3e6c
Zimogor70 <43498740+Zimogor70@users.noreply.github.com>
2024-10-16 20:02:07 +0300

    Задача выполнена

--- 2024.10.16/task01.py
+++ 2024.10.16/task01.py
@@ -3,3 +3,19 @@
 
 Написать функцию для генерации строки из n строчных латинских букв
 """
+
+from random import randint
+import string
+
+def genStr(n:int)->str:
+    g_str:str = ''
+    let = string.ascii_letters
+    for i in range(0,n):        
+        g_str+=let[(randint(0,len(let)-1))]
+    return g_str
+
+def main()->None:
+    print(genStr(15))
+
+if __name__ == '__main__':  
+    main()
